f3aaaed497a3b28b12576c85b94af63f58d751fc
Joy Evans <joyevans@me.com>
2021-01-23 13:33:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// ERROR 1) index.js companyName is set as Scuber: ReferenceError: companyName is not defined
+// let companyName = 'Scuber'; (ERROR 1 solution)
+
+// ERROR 2) index.js companyName is defined as a const:
+const companyName = 'Scuber';
+
+// ERROR 3) index.js mostProfitableNeighborhood is declared as equal to Chelsea: ReferenceError: mostProfitableNeighborhood is not defined
+let mostProfitableNeighborhood = 'Chelsea'
+
+// ERROR 4) index.js companyCeo is declared as equal to Susan Smith: ReferenceError: companyCeo is not defined
+let companyCeo = 'Susan Smith'
